ff0fee752b89d2217685484fc7ee256b4088bb41
scepter914 <st14.828soccer@gmail.com>
2020-07-27 23:55:08 +0900

    initial commit

---
+++ conf.d/set_color.fish
@@ -0,0 +1,28 @@
+# set color
+set -g fish_color_normal normal
+set -g fish_color_command 00ff87
+set -g fish_color_quote ff8700
+set -g fish_color_redirection ff5f00
+set -g fish_color_end 009900
+set -g fish_color_error ff0000
+set -g fish_color_param ff8700
+set -g fish_color_comment ff00af
+set -g fish_color_match normal
+set -g fish_color_selection c0c0c0
+set -g fish_color_search_match ffff00
+set -g fish_color_history_current normal
+set -g fish_color_operator 00a6b2
+set -g fish_color_escape 00a6b2
+set -g fish_color_cwd 008000
+set -g fish_color_cwd_root 800000
+set -g fish_color_valid_path normal
+set -g fish_color_autosuggestion d7ffff
+set -g fish_color_user 00ff00
+set -g fish_color_host normal
+set -g fish_color_cancel normal
+
+# set pager color
+set -g fish_pager_color_completion add8e6
+set -g fish_pager_color_selected_completion brblack
+set -g fish_pager_color_selected_background --background=e0ffff
+
